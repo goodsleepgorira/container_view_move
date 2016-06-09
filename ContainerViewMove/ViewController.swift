5a20cc4db64b6763a4ea9a09aed8036ff7045715
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-06-08 21:52:20 -0600

    Initial Commit

---
+++ ContainerViewMove/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContainerViewMove
+//
+//  Created by 齋藤緒 on 2016/06/08.
+//  Copyright © 2016年 TestOrganization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
